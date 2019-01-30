551cc55948e38feaad27926cb01d4d311f7cecd4
z y <zachary_young@zerovector.space>
2019-01-29 19:46:02 -0800

    Created RobotMap.java
    Added motor controller can IDs

---
+++ frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/RobotMap.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc1388.frc2019official;
+
+public class RobotMap
+{
+    public static int Drivetrain_LF = 3;
+    public static int Drivetrain_RF = 2;
+    public static int Drivetrain_LB = 4;
+    public static int Drivetrain_RB = 1;
+}
