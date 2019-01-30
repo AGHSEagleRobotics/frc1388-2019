5efdb6fd913444af34c9c5e87e0aa861eee97d83
z y <zachary_young@zerovector.space>
2019-01-29 20:24:07 -0800

    Comment RobotMap.java

--- frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/RobotMap.java
+++ frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/RobotMap.java
@@ -1,7 +1,13 @@
 package org.usfirst.frc1388.frc2019official;
 
+/*
+ * Use this class to replace Magic Numbers
+ */
 public class RobotMap
 {
+    /*
+     * Drive Train Can IDs
+     */
     public static int Drivetrain_LF = 3;
     public static int Drivetrain_RF = 2;
     public static int Drivetrain_LB = 4;
