1dbbf90eabfa8c0a956c0cf6282b2af0e05d4bb4
Dylan Tran <dylantran12@hotmail.com>
2019-02-04 20:02:41 -0800

    Merged the BallGrabber subsystem and the PancakeMaker subsystems together into a new Manipulator subsystem.
    Merged the ballGrab command and the pancakeMake command together into a new Manipulate command.

---
+++ frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/commands/Manipulate.java
@@ -0,0 +1,150 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc1388.frc2019official.commands;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc1388.frc2019official.Robot;
+import org.usfirst.frc1388.frc2019official.UsbLogging;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class Manipulate extends Command {
+
+  // ballgrab variables
+  private boolean ejectorEnabled = false;
+  private Timer ejectorTimer;
+  private boolean drBothTriggersPressed;
+  private boolean opBothTriggersPressed;
+  private boolean bothTriggersPressed;
+  private boolean leftTriggerPressed;
+  private boolean rightTriggerPressed;
+  private boolean ejectorEnabledAndTimerExpired;
+
+  // pancake variables
+  private boolean dPadUp;
+  private boolean dPadDown;
+
+  public Manipulate() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+
+    requires(Robot.manipulator);
+    // ballgrab variables
+    ejectorEnabled = false;
+    ejectorTimer = new Timer();
+    drBothTriggersPressed = false;
+    opBothTriggersPressed = false;
+    bothTriggersPressed = false;
+    leftTriggerPressed = false;
+    rightTriggerPressed = false;
+    ejectorEnabledAndTimerExpired = false;
+
+    // pancake variables
+    dPadUp = false;
+    dPadDown = false;
+
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+
+    // ballgrab execute
+    // Read Triggers
+    double drRightTrigger = Robot.oi.getDriveController().getTriggerAxis(Hand.kRight);
+    double opRightTrigger = Robot.oi.getOpController().getTriggerAxis(Hand.kRight);
+    double drLeftTrigger = Robot.oi.getDriveController().getTriggerAxis(Hand.kLeft);
+    double opLeftTrigger = Robot.oi.getOpController().getTriggerAxis(Hand.kLeft);
+    // Assign values
+    drBothTriggersPressed = (drRightTrigger > 0 && drLeftTrigger > 0);
+    opBothTriggersPressed = (opRightTrigger > 0 && opLeftTrigger > 0);
+    bothTriggersPressed = (drBothTriggersPressed || opBothTriggersPressed);
+    leftTriggerPressed = (drLeftTrigger > 0 || opLeftTrigger > 0);
+    rightTriggerPressed = (drRightTrigger > 0 || opRightTrigger > 0);
+    ejectorEnabledAndTimerExpired = (ejectorEnabled && ejectorTimer.hasPeriodPassed(2));
+
+    // Grab (close grabber) when Right trigger pressed (eiter controller)
+    if (rightTriggerPressed && !bothTriggersPressed && !ejectorEnabled) {
+      // Grab (close Grabber)
+      Robot.manipulator.ballGrab();
+    }
+
+    // Release (open grabber) when Left trigger pressed (either controller)
+    if (leftTriggerPressed && !bothTriggersPressed) {
+      Robot.manipulator.ballRelease();
+    }
+
+    // Eject Ball (open grabber and extend ejector) when both triggers pressed
+    // (either controller)
+    if (bothTriggersPressed) {
+      Robot.manipulator.ballRelease();
+      Robot.manipulator.ballEjectorExtend();
+      // set an ejector enabled flag
+      ejectorEnabled = true;
+      // start a timer
+      ejectorTimer.start();
+    }
+
+    // Retract the ejector afater two seconds and both triggers are released (either
+    // controller)
+    if (ejectorEnabledAndTimerExpired && !bothTriggersPressed) {
+      // Retract the ejector
+      Robot.manipulator.ballEjectorRetract();
+      // reset the ejector enabled flag
+      ejectorEnabled = false;
+      // stop the timer
+      ejectorTimer.stop();
+    }
+
+    // pancake execute
+    int Up = 0;
+    int Down = 180;
+    boolean drDPadUp = Robot.oi.getDriveController().getPOV(0) == Up;
+    boolean opDPadUp = Robot.oi.getOpController().getPOV(0) == Up;
+    boolean drDPadDown = Robot.oi.getDriveController().getPOV(0) == Down;
+    boolean opDPadDown = Robot.oi.getOpController().getPOV(0) == Down;
+
+    dPadUp = (drDPadUp || opDPadUp);
+    dPadDown = (drDPadDown || opDPadDown);
+
+    if (dPadUp && !dPadDown) { // move pancake arm up
+      Robot.manipulator.pancakeUp();
+      UsbLogging.debug("pancake arm up");
+    }
+
+    if (dPadDown && !dPadUp) { // move pancake arm down
+      Robot.manipulator.pancakeDown();
+      UsbLogging.debug("pancake arm down");
+    }
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
