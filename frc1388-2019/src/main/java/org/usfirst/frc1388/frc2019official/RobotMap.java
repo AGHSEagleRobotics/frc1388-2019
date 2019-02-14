1285f94098189aa7d554a4e72931f55b09ff37a8
Team1388 <programming@eaglerobotics.com>
2019-02-13 22:42:43 -0800

    Updates to Elevator subsystem to more closely match this year's robot. -> Final numbers will need to be determined based on the robot design. -> This commit has not been tested.

--- frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/RobotMap.java
+++ frc1388-2019/src/main/java/org/usfirst/frc1388/frc2019official/RobotMap.java
@@ -18,13 +18,14 @@ public class RobotMap
      */
     public static final int CANID_Drivetrain_LB = 4;
     public static final int CANID_Drivetrain_RB = 1;
+    public static final int CANID_elevatorMotor = 55;   // TODO: assign the correct ID
 
     /**
      *   Pneumatics Control Modules
      */
     /* PCM CAN IDs */
-    public static final int CANID_PCM12v = 0;
-    public static final int CANID_PCM24v = 1;
+    public static final int CANID_PCM1 = 1;
+    public static final int CANID_PCM2 = 2;
 
     /* PCM12V channels */
     // public static final int PCMCH_12Vchannel0 = 0;
@@ -36,12 +37,18 @@ public class RobotMap
     public static final int PCMCH_pancakeArm = 6;
 
     /* PCM24V channels */
-    // public static final int PCMCH_24Vchannel0 = 0;
-    // public static final int PCMCH_24Vchannel1 = 1;
+    public static final int PCMCH_towerPush = 99;       // TODO: determine the correct channel
+    public static final int PCMCH_towerPull = 99;       // TODO: determine the correct channel
     // public static final int PCMCH_24Vchannel1 = 2;
     // public static final int PCMCH_24Vchannel1 = 3;
     // public static final int PCMCH_24Vchannel1 = 4;
     // public static final int PCMCH_24Vchannel1 = 5;
     // public static final int PCMCH_24Vchannel1 = 6;
     // public static final int PCMCH_24Vchannel1 = 7;
+
+    /* Digital IO */
+    public static final int DIO_elevatorEncoderA = 1;   // TODO: verify this is correct
+    public static final int DIO_elevatorEncoderB = 2;   // TODO: verify this is correct
+    public static final int DIO_bottomLimit1 = 8;       // TODO: verify this is correct
+    public static final int DIO_bottomLimit2 = 9;       // TODO: verify this is correct
 }
